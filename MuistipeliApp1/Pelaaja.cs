c3325c67af961483d9765b648a2203b5433e0557
Emilia Hakomäki <hakomaki.emilia@gmail.com>
2023-12-18 14:14:48 +0200

    Add files via upload

---
+++ MuistipeliApp1/Pelaaja.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuistipeliApp1
+{
+    public class Pelaaja
+    {
+        public string Etunimi { get; set; }
+        public int Voitot { get; set; }
+        public int Tappiot { get; set; }
+        public int Tasapelit { get; set; }
+    }
+}
